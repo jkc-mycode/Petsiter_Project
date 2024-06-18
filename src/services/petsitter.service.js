2daa1aa8c7950abb1741cb2943ec9d41d15863de
jkc-mycode <jkc9709@naver.com>
2024-06-18 15:58:05 +0900

    add/search petsitter detail

--- src/services/petsitter.service.js
+++ src/services/petsitter.service.js
@@ -50,26 +50,44 @@ export class PetsitterService {
   getPetsitterList = async (orderByCondition) => {
     // 펫시터 목록 조회
     let petsitters = await this.petsitterRepository.getPetsitterList(orderByCondition);
-    if (!petsitters) throw new HttpError.NotFound('펫시터가 존재하지 않습니다.');
+    if (!petsitters) throw new HttpError.NotFound('해당 펫시터가 존재하지 않습니다.');
 
     // 펫시터 출력 양식 설정
     petsitters = petsitters.map((petsitter) => {
       return {
         petsitterId: petsitter.petsitterId,
-        petsitterName: petsitter.petsitterName,
-        petsitterCareer: petsitter.petsitterCareer,
-        petsitterProfileImage: petsitter.petsitterProfileImage,
         title: petsitter.title,
-        content: petsitter.content,
         region: petsitter.region,
         total_rate: petsitter.total_rate,
+        review_count: petsitter.review.length,
         createdAt: petsitter.createdAt,
         updatedAt: petsitter.updatedAt,
-        certificate: petsitter.certificate,
         houseImage: petsitter.houseImage,
       };
     });
 
     return petsitters;
+  };
+
+  // 펫시터 상세 조회
+  getPetsitterDetail = async (petsitterId) => {
+    let petsitter = await this.petsitterRepository.getPetsitterDetail(petsitterId);
+    if (!petsitter) throw new HttpError.NotFound('해당 펫시터가 존재하지 않습니다.');
+
+    return {
+      petsitterId: petsitter.petsitterId,
+      petsitterName: petsitter.petsitterName,
+      petsitterCareer: petsitter.petsitterCareer,
+      petsitterProfileImage: petsitter.petsitterProfileImage,
+      title: petsitter.title,
+      content: petsitter.content,
+      region: petsitter.region,
+      total_rate: petsitter.total_rate,
+      createdAt: petsitter.createdAt,
+      updatedAt: petsitter.updatedAt,
+      certificate: petsitter.certificate,
+      houseImage: petsitter.houseImage,
+      review: petsitter.review,
+    };
   };
 }
