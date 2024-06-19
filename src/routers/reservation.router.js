58f841741a62ec429f1c1cfa7a145ddb6b06a42c
yooseungyeop <yooseungyub@naver.com>
2024-06-19 16:07:25 +0900

    refactor/reservation controller service

--- src/routers/reservation.router.js
+++ src/routers/reservation.router.js
@@ -1,26 +1,40 @@
 import express from 'express';
-import reservationController from '../controllers/reservation.controller.js';
+import ReservationController from '../controllers/reservation.controller.js';
 import accesstokenmiddleware from '../middlewares/access-token.middleware.js';
+import { prisma } from '../utils/prisma.util.js';
+import ReservationRepository from '../repositories/reservation.repository.js';
+import ReservationService from '../services/reservation.service.js';
 
 const reservationRouter = express.Router();
+const reservationRepository = new ReservationRepository(prisma);
+const reservationService = new ReservationService(reservationRepository);
+const reservationController = new ReservationController(reservationService);
 
 // 예약 생성
-reservationRouter.post(
-  '/',
-  accesstokenmiddleware,
-  reservationController.createReservationController
-);
+reservationRouter.post('/', accesstokenmiddleware, reservationController.createReservation);
 
 // 예약 목록 조회
-reservationRouter.get('/', reservationController.getReservationController);
+reservationRouter.get('/', accesstokenmiddleware, reservationController.getReservation);
 
 // 예약 상세 조회
-reservationRouter.get('/:reservationId', reservationController.getReservationByIdController);
+reservationRouter.get(
+  '/:reservationId',
+  accesstokenmiddleware,
+  reservationController.getReservationById
+);
 
 // 예약 수정
-reservationRouter.put('/:reservationId', reservationController.updateReservationController);
+reservationRouter.put(
+  '/:reservationId',
+  accesstokenmiddleware,
+  reservationController.updateReservation
+);
 
 // 예약 삭제
-reservationRouter.delete('/:reservationId', reservationController.deleteReservationController);
+reservationRouter.delete(
+  '/:reservationId',
+  accesstokenmiddleware,
+  reservationController.deleteReservation
+);
 
 export default reservationRouter;
