d802757cbf8bf5a89f82a6f555f7a3220e597c86
jkc-mycode <jkc9709@naver.com>
2024-06-20 16:50:08 +0900

    fix/multer error

---
+++ src/constants/multer-message.constant.js
@@ -0,0 +1,10 @@
+export const MULTER_MESSAGE = {
+  PART_COUNT: '필드와 파일 수의 총합이 너무 많습니다.',
+  FILE_SIZE: '파일 용량이 너무 큽니다.',
+  FILE_COUNT: '파일 수가 너무 많습니다.',
+  FIELD_KEY: '필드의 이름이 너무 깁니다.',
+  FIELD_VALUE: '필드의 값이 너무 깁니다.',
+  FIELD_COUNT: '필드가 너무 많습니다.',
+  UNEXPECTED_FILE: '지원하지 않은 파일입니다.',
+  FIELD_NAME: '필드의 이름을 읽어버렸습니다.',
+};
